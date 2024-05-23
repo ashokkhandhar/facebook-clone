d0e07436145099f599e6a722d3c570f5a698cdca
ashok khandhar <ashokkhandhar18@gmail.com>
2024-05-23 18:24:09 +0530

    add api url

--- frontend/src/api_url.jsx
+++ frontend/src/api_url.jsx
@@ -1,4 +1,4 @@
-const API_URL="https://fb-clone-api.vercel.app/api";
+const API_URL="https://facebook-clone-api-plum.vercel.app/api";
 // const API_URL="http://localhost:3000/api"
 
 export default API_URL;
